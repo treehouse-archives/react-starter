c1b3b81ae5e9b0e38dc695a7f7de46aa228b432e
Sijal Manandhar <sijal.m06@gmail.com>
2024-11-09 21:35:55 +0545

    all files formatted & updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,16 @@
-import { Link } from 'react-router-dom';
-import ThemeToggler from './ThemeToggler';
+import { Link } from "react-router-dom";
+import ThemeToggler from "./ThemeToggler.jsx";
 
 const Header = () => {
   return (
     <header>
-      <div className='p-6 flex-between'>
-        <ul className='flex-between space-x-5'>
-          <li className='font-medium text-lg font-mono dark:text-slate-200 text-slate-700'>
-            <Link to={'/'}>Home</Link>
+      <div className="p-6 flex-between">
+        <ul className="flex-between space-x-5">
+          <li className="font-medium text-lg font-mono dark:text-slate-200 text-slate-700">
+            <Link to={"/"}>Home</Link>
           </li>
-          <li className='font-medium text-lg font-mono dark:text-slate-200 text-slate-700'>
-            <Link to={'/about'}>About</Link>
+          <li className="font-medium text-lg font-mono dark:text-slate-200 text-slate-700">
+            <Link to={"/about"}>About</Link>
           </li>
         </ul>
         <ThemeToggler />
