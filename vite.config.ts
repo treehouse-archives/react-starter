716fafbd46efee6777e3f5a6644efc6d73a8ba5b
Sijal Manandhar <sijal.m06@gmail.com>
2025-04-06 07:32:24 +0545

    updated `vite.config`

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { fileURLToPath } from "url";
+import { fileURLToPath } from "node:url";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
